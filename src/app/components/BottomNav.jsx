01c8f5c1713cd680f4e360890d8379a39f483a26
Isuu1 <jakub.babelek@gmail.com>
2024-09-26 14:55:11 +0100

    Changed fetch link for quotes

--- src/app/components/BottomNav.jsx
+++ src/app/components/BottomNav.jsx
@@ -1,15 +1,21 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Goal, Dumbbell, Home, GlassWater, SmilePlus } from 'lucide-react';
-import toast, { Toaster } from 'react-hot-toast';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Goal,
+  Dumbbell,
+  Home,
+  GlassWater,
+  SmilePlus,
+} from "lucide-react";
+import toast, { Toaster } from "react-hot-toast";
 
 const navItems = [
-  { href: '/goal-setter', icon: Goal, label: 'Goal' },
-  { href: '/workout-tracker', icon: Dumbbell, label: 'Workout' },
-  { href: '/', icon: Home, label: 'Home' },
-  { href: '/water-tracker', icon: GlassWater, label: 'Water' },
+  { href: "/goal-setter", icon: Goal, label: "Goal" },
+  { href: "/workout-tracker", icon: Dumbbell, label: "Workout" },
+  { href: "/", icon: Home, label: "Home" },
+  { href: "/water-tracker", icon: GlassWater, label: "Water" },
 ];
 
 export default function BottomNav() {
@@ -17,34 +23,32 @@ export default function BottomNav() {
 
   const fetchMotivationalQuote = async () => {
     try {
-      const response = await fetch(
-        'https://api.quotable.io/random?tags=motivational'
-      );
+      const response = await fetch("https://zenquotes.io/api/random");
       const data = await response.json();
 
       if (response.ok) {
         toast.custom((t) => (
           <div
             className={`${
-              t.visible ? 'animate-enter' : 'animate-leave'
+              t.visible ? "animate-enter" : "animate-leave"
             } max-w-md w-full bg-white border-r-5  shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
           >
-            <div className='flex-1 w-0 p-4'>
-              <div className='flex items-start'>
-                <div className='ml-3 flex-1'>
-                  <p className='text-sm font-medium text-gray-900'>
-                    {data.content}
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start">
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {data.q}
                   </p>
-                  <p className='mt-1 text-sm text-gray-500 text-center'>
-                    {data.author}
+                  <p className="mt-1 text-sm text-gray-500 text-center">
+                    {data.a}
                   </p>
                 </div>
               </div>
             </div>
-            <div className='flex border-l border-gray-200'>
+            <div className="flex border-l border-gray-200">
               <button
                 onClick={() => toast.dismiss(t.id)}
-                className='w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               >
                 Close
               </button>
@@ -52,37 +56,39 @@ export default function BottomNav() {
           </div>
         ));
       } else {
-        toast.error('Failed to fetch motivational quote.');
+        toast.error("Failed to fetch motivational quote.");
       }
     } catch (error) {
-      toast.error('An error occurred while fetching the quote.');
+      toast.error("An error occurred while fetching the quote.");
     }
   };
 
   return (
-    <nav className='sticky bottom-0 left-0 right-0 shadow-lg bg-primaryColour'>
-      <div className='flex gap-12 items-center max-w-md mx-auto'>
+    <nav className="sticky bottom-0 left-0 right-0 shadow-lg bg-primaryColour">
+      <div className="flex gap-12 items-center max-w-md mx-auto">
         {navItems.map((item) => (
           <Link
             key={item.href}
             href={item.href}
             className={
               pathname === item.href
-                ? 'flex flex-col items-center justify-center p-2 rounded-lg bg-teal-500 font-bold'
-                : 'flex flex-col items-center justify-center p-2 rounded-lg text-green-800'
+                ? "flex flex-col items-center justify-center p-2 rounded-lg bg-teal-500 font-bold"
+                : "flex flex-col items-center justify-center p-2 rounded-lg text-green-800"
             }
           >
-            <item.icon className='h-6 w-6 text-white' />
-            <span className='text-xs mt-1 text-white'>{item.label}</span>
+            <item.icon className="h-6 w-6 text-white" />
+            <span className="text-xs mt-1 text-white">
+              {item.label}
+            </span>
           </Link>
         ))}
 
         <button
-          type='button'
-          className='text-white rounded-lg justify-center items-center flex flex-col text-xs'
+          type="button"
+          className="text-white rounded-lg justify-center items-center flex flex-col text-xs"
           onClick={fetchMotivationalQuote}
         >
-          <SmilePlus className='h-6 w-6 text-white' />
+          <SmilePlus className="h-6 w-6 text-white" />
           Motivation
         </button>
       </div>
