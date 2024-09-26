6888b73b520415f7693878365d9edc7c4ca2a0b1
Ellie <ellierparry@gmail.com>
2024-09-26 13:34:34 +0100

    Styling added for workout, water input, and goal setting.

--- src/app/water-tracker/page.jsx
+++ src/app/water-tracker/page.jsx
@@ -1,48 +1,52 @@
-'use client';
+'use client'
 
 import React, { useState } from 'react';
-import TextBox from '../components/TextBox';
+import TextBox from '../components/TextBox'
+import { GlassWater } from 'lucide-react';
 import Nav from '../components/Nav';
 import BottomNav from '../components/BottomNav';
 
 export default function WaterTracker() {
-  const handleValueChange = (value) => {
-    console.log('Litres entered:', value);
-  };
 
-  return (
-    <>
-      <Nav />
-      <div className='wrapper'>
-        <div className='heading'>
-          <h1>Water Intake:</h1>
-        </div>
+    const handleValueChange = (value) => {
+        console.log('Litres entered:', value);
+      };
 
-        <div className='input-section'>
-          <div className='workout-type'>
-            <p>Daily Water Intake</p>
-            <TextBox
-              label='Input minutes:'
-              placeholder='Enter litres'
-              onValueChange={handleValueChange}
-            />
-          </div>
+    return (
+        <>
+        <Nav />
+            <div className="bg-applegreen flex-col space-y-20 min-h-screen flex items-center justify-center bg-white-500 px-4 pb-20 sm:px-6 lg:px-8">
 
-          <div className='duration'>
-            <p>Daily Water Goal</p>
-            <TextBox
-              label='Input minutes:'
-              placeholder='Enter litres'
-              onValueChange={handleValueChange}
-            />
-          </div>
-        </div>
+                <div className="heading text-center">
+                    <div className="flex justify-center">
+                        <GlassWater className="text-teal-400" size={80} />
+                    </div>
+                    <h1 className="mt-2 text-4xl font-bold text-offblack">Water Intake</h1>
+                </div>
 
-        <div className='submit-btn'>
-          <button>Submit</button>
-        </div>
-      </div>
-      <BottomNav />
-    </>
-  );
+            <div className="flex-col space-y-2">
+                <div className="bg-offwhite text-center rounded-t-2xl px-10 py-3 space-y-4">
+                    <div className="block w-full rounded-md space-y-4">
+                        <p className="mt-2 text-2xl text-fitbitgrey">Daily Water Intake</p>
+                        <TextBox className="block w-full px-3 py-2 pl-10 border  placeholder-primaryColour text-primaryColour rounded-full focus:outline-none focus:ring-teal-400 focus:border-teal-400 focus:z-10 sm:text-sm block" placeholder="Enter litres" onValueChange={handleValueChange} />
+                    </div>
+                    </div>
+
+                    <div className="bg-offwhite text-center rounded-b-2xl px-10 pt-1 pb-5 space-y-1">
+                        <div>
+                        <p className="mt-2 text-2xl text-fitbitgrey">Daily Water Goal</p>
+                        <TextBox className="block w-full px-3 py-2 pl-10 border  placeholder-primaryColour text-primaryColour rounded-full focus:outline-none focus:ring-teal-400 focus:border-teal-400 focus:z-10 sm:text-sm block" placeholder="Enter litres" onValueChange={handleValueChange} />
+    
+                    </div>
+                </div>
+                </div>
+
+                <div className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-teal-300 hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-400">
+                    <button>Submit</button>
+                </div>
+
+            </div>
+            <BottomNav />
+        </>
+    )
 }
